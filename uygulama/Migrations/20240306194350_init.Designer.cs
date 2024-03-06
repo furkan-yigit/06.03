d75515aec5e74f5e2b7d92e134810f0215b54c6e
furkan-yigit <fyigit98@outlook.com>
2024-03-06 22:59:12 +0300

    dropdown-list

--- uygulama/Migrations/20240306194350_init.Designer.cs
+++ uygulama/Migrations/20240306194350_init.Designer.cs
@@ -12,11 +12,7 @@ using uygulama.Models.Context;
 namespace uygulama.Migrations
 {
     [DbContext(typeof(CafeDbContext))]
-<<<<<<<< HEAD:uygulama/Migrations/20240306190830_init.Designer.cs
-    [Migration("20240306190830_init")]
-========
-    [Migration("20240306185605_init")]
->>>>>>>> origin/omer:uygulama/Migrations/20240306185605_init.Designer.cs
+    [Migration("20240306194350_init")]
     partial class init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
