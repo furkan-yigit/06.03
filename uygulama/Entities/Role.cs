ec69cfe5ab5265242d87c1990bfd19b2ca803c72
furkan-yigit <fyigit98@outlook.com>
2024-03-07 14:28:36 +0300

    seed-data

---
+++ uygulama/Entities/Role.cs
@@ -0,0 +1,10 @@
+﻿namespace uygulama.Entities
+{
+    public class Role
+    {
+        public int ID { get; set; }
+        public string UserRole { get; set; }
+        public string Description { get; set; }
+        public List<UserRole> UserRoles { get; set; }
+    }
+}
